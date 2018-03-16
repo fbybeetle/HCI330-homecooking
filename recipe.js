51aa68e6258c8c35526d0a1c4a427ec7b5a51b5c
sunghyukpark <sunghyukpark0408@gmail.com>
2018-03-16 10:26:06 -0500

    Redesign new recipe detail page

--- recipe.js
+++ recipe.js
@@ -1,3 +1,8 @@
 function addToMyFavorites(){
-	document.getElementById("add_to_favorite").innerHTML = "Successfully Added";
+	var el = document.getElementById("add_to_favorite");
+	if (el.innerHTML.indexOf("Favorites") != -1){
+		el.innerHTML = "Successfully Added";
+	} else {
+		el.innerHTML = "Add to My Favorites";
+	}
 }
